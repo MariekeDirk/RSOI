f4876a540c021155df7a7de4a509a03723b83078
MariekeDirk <marieke.dirksen@knmi.nl>
2019-10-02 10:09:26 +0000

    added package structure

--- R/data.R
+++ R/data.R
@@ -1,8 +1,8 @@
-#' Monthly temperature observations from the Netherlands 
+#' Monthly temperature observations from the Netherlands
 #'
 #' @description This dataset includes monthly mean temperature observations from the Netherlands since 1906. Coordinates are stored
-#' as data attributes, with "x" as LON and "y" as LAT in WGS84. 
-#' 
+#' as data attributes, with "x" as LON and "y" as LAT in WGS84.
+#'
 #' @docType data
 #'
 #' @usage data(TN_observations)
@@ -10,7 +10,8 @@
 #'
 #' @keywords datasets
 #' @author Marieke Dirksen
-#' @examples 
+#' @export
+#' @examples
 #' data(TN_observations)
 #' head(TN_observations)
 #' attr(TN_observations,'x')
